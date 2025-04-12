8aca8b542724a40481c1db56fc93612a9a736fe1
Edoardo <evicenzi357@gmail.com>
2025-04-12 21:23:25 +0200

    Add fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,9 +11,38 @@ function fish_greeting
     echo "man!"
 end
 
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    fastfetch
+end
+
+starship init fish | source
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
+end
+
+alias pamcan=pacman
 alias v "nvim"
 alias fishy "nvim ~/.config/fish/config.fish"
 alias home "cd ~"
 
 set -gx EDITOR /usr/bin/nvim
 set -gx GIT_EDITOR /usr/bin/nvim
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
